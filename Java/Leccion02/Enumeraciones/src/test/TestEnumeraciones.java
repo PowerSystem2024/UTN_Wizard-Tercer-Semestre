05f3e167ecaa391984c6080c091a30d3dd9e0f93
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-05-21 23:53:45 -0300

    1.3 Pruebas de enum, con la creación de enum Continentes

--- Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -1,10 +1,30 @@
 
 package test;
 
+import enumeraciones.Continentes;
 import enumeraciones.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia 1: "+Dias.LUNES);
+        //System.out.println("Dia 1: "+Dias.LUNES);
+        //indicarDiasSemana(Dias.MARTES); //Las enumeraciones se tratan como cadenas 
+        // Ahora no se deben utilizar comillas, se accede a traves de el operador de puntos
+        System.out.println("Continente No. 4: "+Continentes.AMERICA); 
+        System.out.println("No. de paises en el 4to. continente: "+Continentes.AMERICA.getPaises());
+        System.out.println("No. de habitantes en el 4to. continente: "+Continentes.AMERICA.getHabitantes());
+        
+    }   
+    private static void indicarDiasSemana(Dias dias){
+        switch(dias){
+                case LUNES:
+                    System.out.println("Primer dia de la semana");
+                     break;
+                case MARTES:
+                     System.out.println("Segundo dia de la semana");
+                     break;
+                     //Agregar todos los dias de la semana
+                     //Agregar default 
+        }
     }
+    
 }

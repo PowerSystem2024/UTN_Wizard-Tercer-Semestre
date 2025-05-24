16dfaab45d189b5b520b441fa44e033384ad630a
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-05-23 22:33:02 -0300

    1.3 Modificadores de acceso public

---
+++ Java/Leccion03/ModificadorAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,14 @@
+
+package paquete1;
+
+
+public class Clase1 {
+    public String atributoPublic = "Valor de atributo public";
+    
+    public Clase1(){
+        System.out.println("Constructor publica");
+    }
+    public void metodoPublico(){
+        System.out.println("Metodo publico");
+    }
+}

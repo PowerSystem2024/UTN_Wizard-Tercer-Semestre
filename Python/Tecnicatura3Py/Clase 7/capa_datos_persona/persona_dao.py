bd46150d5c2565d044d6b6dbd0b87cd39db3e479
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-02 20:24:34 -0300

    Clase 9 Capa de Datos Parte 3 9.1 En la clase PersonaDao: método seleccionar

--- Python/Tecnicatura3Py/Clase 7/capa_datos_persona/persona_dao.py
+++ Python/Tecnicatura3Py/Clase 7/capa_datos_persona/persona_dao.py
@@ -7,7 +7,19 @@ class PersonaDao:
                     Update  -> Actualizar
                     Delate  -> Eliminar
     """
-_SELECCIONAR = 'SELECT * FROM persona ORDEN BY id_persona'
-_INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
-_ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-_ELIMINAR = 'DELATE FROM persona WHERE id_persona=%s'
+    _SELECCIONAR = 'SELECT * FROM persona ORDEN BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELATE FROM persona WHERE id_persona=%s'
+
+    # Definimos los metodos de clase
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                persona = [] # creamos una lista
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    persona.append(persona)

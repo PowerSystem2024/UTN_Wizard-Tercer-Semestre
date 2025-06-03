bba72496e34d16ed9b77dc0e84711b30521de0ba
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-02 22:02:40 -0300

    9.3 Método Insertar

--- Python/Tecnicatura3Py/Clase 7/capa_datos_persona/conexion.py
+++ Python/Tecnicatura3Py/Clase 7/capa_datos_persona/conexion.py
@@ -1,10 +1,10 @@
 import psycopg2 as bd
-#psycopg2 as bd
 from logger_base import log
 import sys
+
 class Conexion:
-    _DATABASE ='test_bd'
-    _USERNAME ='postgres'
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
@@ -20,26 +20,23 @@ class Conexion:
                                            password=cls._PASSWORD,
                                            port=cls._DB_PORT,
                                            database=cls._DATABASE)
-                log.debug(f'Conexion Exitosa: {cls._conexion}')
-                return cls._conexion
+                log.debug(f'Conexión Exitosa: {cls._conexion}')
             except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
+                log.error(f'Ocurrió un error al conectarse: {e}')
                 sys.exit()
-            else:
-                return cls._conexion
-@classmethod
-def obtenerCursor(cls):
-    if cls._cursor is None:
-        try:
-            cls._cursor = cls.obtenerConexion().cursor()
-            log.debug(f'Se abrio correctamente el cursor: {cls.cursor}')
-            return cls._cursor
-        except Exception as e:
-            log.error(f'Ocurrio un error: {e}')
-            sys.exit()
-        else:
-            return cls._cursor
+        return cls._conexion
 
-if __name__== '__main__':
+    @classmethod
+    def obtenerCursor(cls):
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
+            except Exception as e:
+                log.error(f'Ocurrió un error al abrir el cursor: {e}')
+                sys.exit()
+        return cls._cursor
+
+if __name__ == '__main__':
     Conexion.obtenerConexion()
-    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
--- Python/Tecnicatura3Py/Clase 7/capa_datos_persona/persona_dao.py
+++ Python/Tecnicatura3Py/Clase 7/capa_datos_persona/persona_dao.py
@@ -1,3 +1,6 @@
+from capa_datos_persona.persona import Persona
+from capa_datos_persona.conexion import Conexion
+from logger_base import log
 class PersonaDao:
     """
     DAO significa: Data Access Object
@@ -23,3 +26,35 @@ class PersonaDao:
                 for registro in registros:
                     persona = Persona(registro[0], registro[1], registro[2], registro[3])
                     persona.append(persona)
+                return persona
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerCursor() as cursor:
+            valores = (persona.nombre, persona.apellido, persona.email)
+            cursor.execute(cls._INSERTAR, valores)
+            log.debug(f'Persona Insertada: {persona}')
+            return cursor.rewcount
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona. apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rewcount
+
+if __name__ == '__main__' :
+    # Actualizar un registro
+    persona1 = Persona(1, 'Juan jose', 'Pena', 'jasdiausd@mail.com')
+    personas_actualizas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actulizadas: {peronas_actualizadas}')
+
+
+    # Insertar un registro
+    #persona1 = Persona(nombre='Pedro', apellido='Romero', email='promereoasd@mail.com')
+    #personas_insertadas = PersonaDAO.insertar(persona1)
+    #log.debug(f'Persona insertadas: {personas_insertadas}')
+    # Seleccionar objetos
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)

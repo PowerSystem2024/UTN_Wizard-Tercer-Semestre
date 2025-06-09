345e0af71a3b4ae0273bbd9b8aa88c6b8abf331b
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-09 19:21:49 -0300

    Clase 10 Pool de conexiones con Python y Postgresql -> Parte 1 10.1 POOL DE CONEXIONES CON PYTHON Y POSTGRESQL: Parte 1 y 2

--- Python/Tecnicatura3Py/Clase 7/capa_datos_persona/conexion.py
+++ Python/Tecnicatura3Py/Clase 7/capa_datos_persona/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 from logger_base import log
 import sys
 
@@ -8,35 +8,24 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
-                log.debug(f'Conexión Exitosa: {cls._conexion}')
-            except Exception as e:
-                log.error(f'Ocurrió un error al conectarse: {e}')
-                sys.exit()
-        return cls._conexion
+        pass
 
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
+        pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
             try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
-            except Exception as e:
-                log.error(f'Ocurrió un error al abrir el cursor: {e}')
-                sys.exit()
-        return cls._cursor
+                cls._pool = pool.SimpleConnectionPool()
+
 
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass

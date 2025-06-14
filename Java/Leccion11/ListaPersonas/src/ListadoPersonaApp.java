88ff7c7ad4c73c948a83111d4a1d1d6dbe79f633
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-13 23:16:49 -0300

    10.3 Creamos el Menu -> Parte 1 y 2

---
+++ Java/Leccion11/ListaPersonas/src/ListadoPersonaApp.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListadoPersonaApp {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // Definimos la lista fuera del ciclo while
+        List<Persona> personas = new ArrayList<>();
+        //Empezamos con el menu
+        var salir = false;
+        while (!salir){
+            mostrarMenu();
+            System.out.println();
+        }//Fin del cliclo while
+    }//Fin metodo main
+
+    private static void mostrarMenu(){
+        //mostrar las opciones
+        System.out.print("""
+                ****Listado de Personas*****
+                1. Agregar
+                2. Listar
+                3. Salir
+                """);
+        System.out.print("Digite una de las opciones: ");
+
+    }
+}

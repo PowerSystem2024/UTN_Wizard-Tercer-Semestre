e5ef7757ab69a6ead87326b37ef6da4f4b7d896a
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-19 22:03:39 -0300

    11.3 Sincronizamos con Maven y agregamos la dependencia -> Parte 1 y 2

--- Java/Leccion12/Proyecto estudiantes Parte 1.sql
+++ Java/Leccion12/Proyecto estudiantes Parte 1.sql
@@ -4,3 +4,9 @@
 SELECT * FROM estudiantes.estudiantes2025;
 -- Insertar estudiantes
 INSERT INTO studiantes2025 (nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2652654549"; "Juna@mail.com");
+-- Update (modificar)
+UPDATE estudiantes2025 SET nombre="Juan Carlos", apellido="Garcia" WHERE indestudiantes2025= 1;
+-- Delete (liminar)
+DELETE FROM estudiantes2025 WHERE indestudiantes2025=1;
+-- Para modificar el indestudiantes2025 y comience en 1
+ALTER TABLE indestudiantes2025 AUTO_INCREMENT = 1;

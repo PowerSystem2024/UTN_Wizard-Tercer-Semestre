3be80e0141e965744c23ebcc2ddd2ec5d2968c3e
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-19 21:54:01 -0300

    Clase 11-A Proyecto estudiantes Parte 1 11.2 Diferentes Querys en MySQL -> Parte 1 y 2

---
+++ Java/Leccion12/Proyecto estudiantes Parte 1.sql
@@ -0,0 +1,6 @@
+
+-- Comenzamos con CRUD: create(inserter), read(leer), update(actualizar), delete(eliminar)
+-- Lista los estudianres (read)
+SELECT * FROM estudiantes.estudiantes2025;
+-- Insertar estudiantes
+INSERT INTO studiantes2025 (nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2652654549"; "Juna@mail.com");

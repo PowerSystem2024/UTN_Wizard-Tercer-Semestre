1ae6188818c0bccecfc5d720423f64fd03dd76c6
Axel Choque <37966213@alumnos.frsr.utn.edu.ar>
2025-06-24 23:53:19 -0300

    Java Clase03 AutoboxingUnboxing
    Java Clase03 AutoboxingUnboxing

--- Java/Clase03/AutoboxingUnboxing/src/test/TestAutoboxingUnboxing.java
+++ Java/Clase03/AutoboxingUnboxing/src/test/TestAutoboxingUnboxing.java
@@ -4,22 +4,22 @@ package test;
 
 public class TestAutoboxingUnboxing {
     public static void main(String[] args) {
-        //Clase envolventes o Wrapper
+        //Clases envolvente o Wrapper
         /*
-            Clases envolventes de tipo primitivos
-            int = la clase envolvente es integer
-            long = la clase envolvente s long
-            float = la clase envolvente es float
-            double = la clase envolvente es Double
-            boolean = la clase envolvente es Boolean
-            byte = la clase envolvente es Byte
-            char = la clase envolvente es character
-            Short = la clase envolvente es Short
+            Clase envolvente de tipo primitivos
+            int = la clase envolvente en integer
+            long = la clase envolvente es Long
+            double = Double
+            booblean = Boolean
+            byte = Byte
+            char = Character
+            short = Short
+            
         */
-        int enteroPrimitivo = 10; //Tipo primitivo
-        System.out.println("enteroPrimitivo = " + enteroPrimitivo);
-        Integer entero = 10; //Tipo Object con la clase Integer
-        System.out.println("entero = " + entero.doubleValue());
+        int enteroPrim = 10; //Tipo primitivo
+        System.out.println("enteroPrim = " + enteroPrim);
+        Integer entero = 10; //Tipo objeto con la clase integer
+        System.out.println("entero = " + entero.doubleValue()); //Autoboxing
         
         int entero2 = entero; //Unboxing
         System.out.println("entero2 = " + entero2);
